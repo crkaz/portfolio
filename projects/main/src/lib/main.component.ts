11c1ee4aa1c819aad0197c910d7ce232297124e2
Zak (Lenovo) <zakcatherall@outlook.com>
2020-11-12 17:44:48 +0000

    Added empty modules

---
+++ projects/main/src/lib/main.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-main',
+  template: `
+    <p>
+      main works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class MainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
