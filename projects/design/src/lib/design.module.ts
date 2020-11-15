9cbf297d0fa124602c48ec818d46f4a66c13f0b5
Zak (Lenovo) <zakcatherall@outlook.com>
2020-11-15 00:38:12 +0000

    Wrapped ngmaterial toolbar in design library and successfully imported into app

--- projects/design/src/lib/design.module.ts
+++ projects/design/src/lib/design.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { DesignComponent } from './design.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
 
 
 @NgModule({
-  declarations: [DesignComponent],
+  declarations: [DesignComponent, ToolbarComponent],
   imports: [
+    MatToolbarModule,
   ],
-  exports: [DesignComponent]
+  exports: [DesignComponent, ToolbarComponent]
 })
 export class DesignModule { }
--- projects/portfolio/src/app/app.module.ts
+++ projects/portfolio/src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DesignModule } from '../../../design/src/public-api';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DesignModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
